a7933f2acdd59601eac0ef0ac90c2a840a46c526
Chloe Mouret <chloe.mouret62@orange.fr>
2020-05-03 17:02:41 +0200

    AJax is not a function errror

--- app.py
+++ app.py
@@ -1,12 +1,32 @@
+import flask
 from flask import Flask
 import wikipediaapi
 import requests as rq
+import json
 
 app = Flask(__name__)
+app.jinja_env.trim_blocks = True
+app.jinja_env.lstrip_blocks = True
 
 @app.route('/')
-def hello_world():
-    return get_omdb_data("Inception")
+def home():
+    return flask.render_template("page_film.html.jinja2")
+
+
+@app.route('/getStats/<movie>')
+def get_stats(movie):
+    omdb = get_omdb_data(movie)
+    reponse ={ "Title": movie,
+             "Image": omdb["Poster"],
+             "Year": omdb["Year"],
+             "Actors": omdb["Actors"],
+             "Director": omdb["Director"],
+             "Genre": omdb["Genre"],
+             "Runtime": omdb["Runtime"],
+             "Awards": omdb["Awards"],
+             "Description": get_description(movie)
+             }
+    return json.dumps(reponse)
 
 
 def get_description(movie):
@@ -19,13 +39,14 @@ def get_omdb_data(movie):
     api_key = get_api_key()
     url = "http://www.omdbapi.com/?apikey={}&t={}".format(api_key, movie)
     r = rq.get(url)
-    desc = r.json()["Ratings"][0]["Value"]
-    return desc
+    return r.json()
 
 
 def get_api_key():
     with open("omdb_key.txt", "r") as f:
         return f.read().strip('\n')
+
+
 
 
 
